210e1b42579c2104b9462ae4d4b42b136c1bf000
Marcelo Júnior <rokermarcelo@gmail.com>
2020-04-26 15:54:46 -0300

    Entendido como são determinadas as relações de posicionamento em um Contraint Layout

---
+++ app/src/main/java/com/mimmarcelo/aluraviagens/ui/activity/TripListActivity.java
@@ -0,0 +1,16 @@
+package com.mimmarcelo.aluraviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mimmarcelo.aluraviagens.R;
+
+public class TripListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_list);
+    }
+}
