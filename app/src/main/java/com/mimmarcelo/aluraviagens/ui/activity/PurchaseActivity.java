a129097c248484d0dff4105a547d3b18ae96196a
Marcelo Júnior <rokermarcelo@gmail.com>
2020-04-30 15:35:25 -0300

    Criada tela de resumo da compra

--- app/src/main/java/com/mimmarcelo/aluraviagens/ui/activity/PurchaseActivity.java
+++ app/src/main/java/com/mimmarcelo/aluraviagens/ui/activity/PurchaseActivity.java
@@ -1,25 +1,28 @@
 package com.mimmarcelo.aluraviagens.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.mimmarcelo.aluraviagens.M;
 import com.mimmarcelo.aluraviagens.R;
 import com.mimmarcelo.aluraviagens.model.Trip;
 
-public class PaymentActivity extends AppCompatActivity {
+public class PurchaseActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Trip trip;
     private TextView price;
+    private Button btnPurchase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payment);
+        setContentView(R.layout.activity_purchasing);
         setTitle(R.string.header_of_payment_activity);
 
         getViews();
@@ -29,10 +32,12 @@ public class PaymentActivity extends AppCompatActivity {
 
     private void bindViews() {
         price.setText(trip.getPriceResource());
+        btnPurchase.setOnClickListener(this);
     }
 
     private void getViews() {
         this.price = findViewById(R.id.txt_trip_price);
+        this.btnPurchase = findViewById(R.id.btn_purchasing);
     }
 
     private Trip checkTrip() {
@@ -43,4 +48,11 @@ public class PaymentActivity extends AppCompatActivity {
         return (Trip) getIntent().getSerializableExtra(M.extra.trip_obj);
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, PurchaseResumeActivity.class);
+        intent.putExtra(M.extra.trip_obj, trip);
+        startActivity(intent);
+        finish();
+    }
 }
--- app/src/main/java/com/mimmarcelo/aluraviagens/ui/activity/PurchaseResumeActivity.java
+++ app/src/main/java/com/mimmarcelo/aluraviagens/ui/activity/PurchaseResumeActivity.java
@@ -1,6 +1,5 @@
 package com.mimmarcelo.aluraviagens.ui.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,26 +13,33 @@ import com.mimmarcelo.aluraviagens.M;
 import com.mimmarcelo.aluraviagens.R;
 import com.mimmarcelo.aluraviagens.model.Trip;
 
-public class TripResumeActivity extends AppCompatActivity implements View.OnClickListener {
+public class PurchaseResumeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Trip trip;
 
     public ImageView image;
     public TextView local;
-    public TextView days;
     public TextView price;
     private TextView period;
-    private Button btnPayment;
+    private Button btnBackToList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trip_resume);
-        setTitle(R.string.header_of_resume_activity);
+        setContentView(R.layout.activity_purchase_resume);
+        setTitle(getString(R.string.header_of_purchase_resume_activity));
 
-        getViews();
         this.trip = checkTrip();
+        getViews();
         bindViews();
+    }
+
+    private void bindViews() {
+        image.setImageResource(trip.getImageResource(this));
+        local.setText(trip.getLocal());
+        price.setText(trip.getPriceResource());
+        period.setText(trip.getPeriodResource());
+        btnBackToList.setOnClickListener(this);
     }
 
     private Trip checkTrip() {
@@ -44,29 +50,18 @@ public class TripResumeActivity extends AppCompatActivity implements View.OnClic
         return (Trip) getIntent().getSerializableExtra(M.extra.trip_obj);
     }
 
-    private void bindViews() {
-        image.setImageResource(trip.getImageResource(this));
-        local.setText(trip.getLocal());
-        days.setText(trip.getDaysResource());
-        price.setText(trip.getPriceResource());
-        period.setText(trip.getPeriodResource());
-        btnPayment.setOnClickListener(this);
-    }
-
     private void getViews() {
         this.image = findViewById(R.id.img_banner_trip);
         this.local = findViewById(R.id.txt_trip_local);
-        this.days = findViewById(R.id.txt_trip_days);
         this.price = findViewById(R.id.txt_trip_price);
         this.period = findViewById(R.id.txt_trip_period);
-        this.btnPayment = findViewById(R.id.btn_make_payment);
+        this.btnBackToList = findViewById(R.id.btn_back_to_list);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(this, PurchaseActivity.class);
-        intent.putExtra(M.extra.trip_obj, trip);
-        startActivity(intent);
+//        Intent intent = new Intent(this, TripListActivity.class);
+//        startActivity(intent);
         finish();
     }
 }
